28648183dd3000ec435493c9c5f29c0e3ae55491
spadewh <117936202+spadewh@users.noreply.github.com>
2022-11-11 02:28:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,14 +29,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2002', date: '04-14',
+          type: '生日', name: '郝盛琪', year: '2002', date: '05-25',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝贝', year: '2002', date: '05-25',
+          type: '生日', name: '王翀翊', year: '1998', date: '09-21',
         },
         {
-          type: '节日', name: '在一起', year: '2020', date: '11-14',
+          type: '节日', name: '恋爱纪念日', year: '2020', date: '11-14',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
